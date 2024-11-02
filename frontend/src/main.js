1a5f2ec5517965416299b83cac79f78833389d7b
cubewhy <61075476+cubewhy@users.noreply.github.com>
2024-11-02 18:41:40 +0800

    i18n

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,18 @@
-import './assets/main.css'
+import {createApp} from 'vue'
+import {createI18n} from 'vue-i18n'
+import en from './locales/en-US.json';
+import zh from './locales/zh-CN.json';
 
-import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const i18n = createI18n({
+    locale: 'en', // 设置默认语言
+    messages: {
+        en: en,
+        zh: zh,
+    },
+})
+
+let app = createApp(App);
+app.use(i18n)
+app.mount('#app')
