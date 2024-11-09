e906bc0e79673ffc1f0a67919dcfd13fe6a5caf3
cubewhy <61075476+cubewhy@users.noreply.github.com>
2024-11-09 17:53:25 +0800

    feat: i18n

--- authorization-server/src/main/java/org/qbynet/authorization/config/SecurityConfig.java
+++ authorization-server/src/main/java/org/qbynet/authorization/config/SecurityConfig.java
@@ -4,10 +4,13 @@ import org.qbynet.authorization.federation.FederatedIdentityAuthenticationSucces
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 @Configuration
 public class SecurityConfig {
@@ -21,12 +24,17 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(conf -> conf
-                        .requestMatchers("/assets/**", "/user/login").permitAll()
+                        .requestMatchers("/assets/**", "/favicon.ico").permitAll()
+                        .requestMatchers("/user/login").anonymous()
                         .anyRequest().authenticated()
                 )
                 .formLogin(conf -> conf
                         .loginPage("/user/login")
                         .loginProcessingUrl("/user/login")
+                )
+                .logout(conf -> conf
+                        .logoutUrl("/user/logout")
+                        .logoutSuccessUrl("/user/login?logout")
                 )
                 .oauth2Login(conf -> conf
                         .loginPage("/user/login")
@@ -39,14 +47,14 @@ public class SecurityConfig {
         return new FederatedIdentityAuthenticationSuccessHandler();
     }
 
-//    @Bean
-//    public SessionRegistry sessionRegistry() {
-//        return new SessionRegistryImpl();
-//    }
-//
-//    @Bean
-//    public HttpSessionEventPublisher httpSessionEventPublisher() {
-//        return new HttpSessionEventPublisher();
-//    }
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
+
+    @Bean
+    public HttpSessionEventPublisher httpSessionEventPublisher() {
+        return new HttpSessionEventPublisher();
+    }
 
 }
--- authorization-server/src/main/java/org/qbynet/authorization/controller/UserController.java
+++ authorization-server/src/main/java/org/qbynet/authorization/controller/UserController.java
@@ -1,5 +1,7 @@
 package org.qbynet.authorization.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,5 +12,12 @@ public class UserController {
     @GetMapping("login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("logout")
+    public String logout(HttpSession session, HttpServletRequest request) throws Exception {
+        session.invalidate();
+        request.logout();
+        return "redirect:/user/login?logout";
     }
 }
