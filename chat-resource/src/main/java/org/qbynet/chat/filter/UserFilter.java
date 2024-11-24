d8ec5cac4194db477f51d28f98195fbae1c39fa1
cubewhy <61075476+cubewhy@users.noreply.github.com>
2024-11-24 19:17:00 +0800

    add NotNull annotation

--- chat-resource/src/main/java/org/qbynet/chat/filter/UserFilter.java
+++ chat-resource/src/main/java/org/qbynet/chat/filter/UserFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.jetbrains.annotations.NotNull;
 import org.qbynet.chat.entity.User;
 import org.qbynet.chat.service.UserService;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -19,7 +20,7 @@ public class UserFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain filterChain) throws ServletException, IOException {
         Principal principal = request.getUserPrincipal();
         if (principal == null) {
             filterChain.doFilter(request, response);
