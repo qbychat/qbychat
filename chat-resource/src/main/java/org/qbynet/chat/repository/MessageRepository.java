17c8b265bf20a31657b23de2c45e567e4a82c953
cubewhy <61075476+cubewhy@users.noreply.github.com>
2024-11-30 07:58:51 +0800

    update MessageRepository

--- chat-resource/src/main/java/org/qbynet/chat/repository/MessageRepository.java
+++ chat-resource/src/main/java/org/qbynet/chat/repository/MessageRepository.java
@@ -4,7 +4,10 @@ import org.qbynet.chat.entity.Message;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
+import java.util.List;
+
 @Repository
 public interface MessageRepository extends MongoRepository<Message, String> {
-
+    List<Message> findByExpiresAtNullOrExpiresAtGreaterThan(Instant now);
 }
