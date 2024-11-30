0837a2de116321dcf478ed5f30f22eef38c34f24
cubewhy <61075476+cubewhy@users.noreply.github.com>
2024-11-30 21:31:51 +0800

    feat: show email instead the internal id

--- authorization-server/src/main/java/org/qbynet/authorization/controller/AuthorizationConsentController.java
+++ authorization-server/src/main/java/org/qbynet/authorization/controller/AuthorizationConsentController.java
@@ -16,6 +16,7 @@
 package org.qbynet.authorization.controller;
 
 import jakarta.annotation.Resource;
+import org.qbynet.authorization.service.AccountService;
 import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsent;
@@ -34,100 +35,122 @@ import java.util.*;
 
 @Controller
 public class AuthorizationConsentController {
-	@Resource
-	RegisteredClientRepository registeredClientRepository;
+    @Resource
+    AccountService accountService;
 
-	@Resource
-	OAuth2AuthorizationConsentService authorizationConsentService;
+    @Resource
+    RegisteredClientRepository registeredClientRepository;
 
-	@GetMapping(value = "/oauth2/consent")
-	public String consent(Principal principal, Model model,
-			@RequestParam(OAuth2ParameterNames.CLIENT_ID) String clientId,
-			@RequestParam(OAuth2ParameterNames.SCOPE) String scope,
-			@RequestParam(OAuth2ParameterNames.STATE) String state,
-			@RequestParam(name = OAuth2ParameterNames.USER_CODE, required = false) String userCode) {
+    @Resource
+    OAuth2AuthorizationConsentService authorizationConsentService;
 
-		// Remove scopes that were already approved
-		Set<String> scopesToApprove = new HashSet<>();
-		Set<String> previouslyApprovedScopes = new HashSet<>();
-		RegisteredClient registeredClient = this.registeredClientRepository.findByClientId(clientId);
+    private static Set<ScopeWithDescription> withDescription(Set<String> scopes) {
+        Set<ScopeWithDescription> scopeWithDescriptions = new HashSet<>();
+        for (String scope : scopes) {
+            scopeWithDescriptions.add(new ScopeWithDescription(scope));
+
+        }
+        return scopeWithDescriptions;
+    }
+
+    @GetMapping(value = "/oauth2/consent")
+    public String consent(Principal principal, Model model,
+                          @RequestParam(OAuth2ParameterNames.CLIENT_ID) String clientId,
+                          @RequestParam(OAuth2ParameterNames.SCOPE) String scope,
+                          @RequestParam(OAuth2ParameterNames.STATE) String state,
+                          @RequestParam(name = OAuth2ParameterNames.USER_CODE, required = false) String userCode) {
+
+        // Remove scopes that were already approved
+        Set<String> scopesToApprove = new HashSet<>();
+        Set<String> previouslyApprovedScopes = new HashSet<>();
+        RegisteredClient registeredClient = this.registeredClientRepository.findByClientId(clientId);
         assert registeredClient != null;
         OAuth2AuthorizationConsent currentAuthorizationConsent =
-				this.authorizationConsentService.findById(registeredClient.getId(), principal.getName());
-		Set<String> authorizedScopes;
-		if (currentAuthorizationConsent != null) {
-			authorizedScopes = currentAuthorizationConsent.getScopes();
-		} else {
-			authorizedScopes = Collections.emptySet();
-		}
-		for (String requestedScope : StringUtils.delimitedListToStringArray(scope, " ")) {
-			if (OidcScopes.OPENID.equals(requestedScope)) {
-				continue;
-			}
-			if (authorizedScopes.contains(requestedScope)) {
-				previouslyApprovedScopes.add(requestedScope);
-			} else {
-				scopesToApprove.add(requestedScope);
-			}
-		}
+                this.authorizationConsentService.findById(registeredClient.getId(), principal.getName());
+        Set<String> authorizedScopes;
+        if (currentAuthorizationConsent != null) {
+            authorizedScopes = currentAuthorizationConsent.getScopes();
+        } else {
+            authorizedScopes = Collections.emptySet();
+        }
+        for (String requestedScope : StringUtils.delimitedListToStringArray(scope, " ")) {
+            if (OidcScopes.OPENID.equals(requestedScope)) {
+                continue;
+            }
+            if (authorizedScopes.contains(requestedScope)) {
+                previouslyApprovedScopes.add(requestedScope);
+            } else {
+                scopesToApprove.add(requestedScope);
+            }
+        }
 
-		model.addAttribute("clientId", clientId);
-		model.addAttribute("state", state);
-		model.addAttribute("scopes", withDescription(scopesToApprove));
-		model.addAttribute("previouslyApprovedScopes", withDescription(previouslyApprovedScopes));
-		model.addAttribute("principalName", principal.getName());
-		model.addAttribute("userCode", userCode);
-		if (StringUtils.hasText(userCode)) {
-			model.addAttribute("requestURI", "/oauth2/device_verification");
-		} else {
-			model.addAttribute("requestURI", "/oauth2/authorize");
-		}
+        model.addAttribute("clientId", clientId);
+        model.addAttribute("state", state);
+        model.addAttribute("scopes", withDescription(scopesToApprove));
+        model.addAttribute("previouslyApprovedScopes", withDescription(previouslyApprovedScopes));
+        model.addAttribute("principalName", maskEmail(accountService.findById(principal.getName()).getEmail()));
+        model.addAttribute("userCode", userCode);
+        if (StringUtils.hasText(userCode)) {
+            model.addAttribute("requestURI", "/oauth2/device_verification");
+        } else {
+            model.addAttribute("requestURI", "/oauth2/authorize");
+        }
 
-		return "consent";
-	}
+        return "consent";
+    }
 
-	private static Set<ScopeWithDescription> withDescription(Set<String> scopes) {
-		Set<ScopeWithDescription> scopeWithDescriptions = new HashSet<>();
-		for (String scope : scopes) {
-			scopeWithDescriptions.add(new ScopeWithDescription(scope));
+    private String maskEmail(String email) {
+        String username = email.split("@")[0];
+        String maskedUsername = username;
+        if (username.length() > 2) {
+            maskedUsername = username.charAt(0) +
+                    username.substring(1, username.length() - 1).replaceAll(".", "*") +
+                    username.charAt(username.length() - 1);
+        }
+        String domain = email.split("@")[1];
+        String[] domainParts = domain.split("\\.");
+        String maskedDomain = domainParts[domainParts.length - 1];
+        if (maskedDomain.length() > 2) {
+            maskedDomain = domainParts[0].substring(0, 2) +
+                    domainParts[0].substring(2).replaceAll(".", "*");
+        }
+        return maskedUsername + "@" + maskedDomain + "." + domainParts[1];
+    }
 
-		}
-		return scopeWithDescriptions;
-	}
+    public static class ScopeWithDescription {
+        private static final String DEFAULT_DESCRIPTION = "UNKNOWN SCOPE - We cannot provide information about this permission, use caution when granting this.";
+        private static final Map<String, String> scopeDescriptions = new HashMap<>();
 
-	public static class ScopeWithDescription {
-		private static final String DEFAULT_DESCRIPTION = "UNKNOWN SCOPE - We cannot provide information about this permission, use caution when granting this.";
-		private static final Map<String, String> scopeDescriptions = new HashMap<>();
-		static {
-			scopeDescriptions.put(
-					OidcScopes.PROFILE,
-					"This application will be able to read your profile information."
-			);
-			scopeDescriptions.put(
-					"message.read",
-					"This application will be able to read your message."
-			);
-			scopeDescriptions.put(
-					"message.write",
-					"This application will be able to add new messages. It will also be able to edit and delete existing messages."
-			);
-			scopeDescriptions.put(
-					"user.read",
-					"This application will be able to read your user information."
-			);
-			scopeDescriptions.put(
-					"other.scope",
-					"This is another scope example of a scope description."
-			);
-		}
+        static {
+            scopeDescriptions.put(
+                    OidcScopes.PROFILE,
+                    "This application will be able to read your profile information."
+            );
+            scopeDescriptions.put(
+                    "message.read",
+                    "This application will be able to read your message."
+            );
+            scopeDescriptions.put(
+                    "message.write",
+                    "This application will be able to add new messages. It will also be able to edit and delete existing messages."
+            );
+            scopeDescriptions.put(
+                    "user.read",
+                    "This application will be able to read your user information."
+            );
+            scopeDescriptions.put(
+                    "other.scope",
+                    "This is another scope example of a scope description."
+            );
+        }
 
-		public final String scope;
-		public final String description;
+        public final String scope;
+        public final String description;
 
-		ScopeWithDescription(String scope) {
-			this.scope = scope;
-			this.description = scopeDescriptions.getOrDefault(scope, DEFAULT_DESCRIPTION);
-		}
-	}
+        ScopeWithDescription(String scope) {
+            this.scope = scope;
+            this.description = scopeDescriptions.getOrDefault(scope, DEFAULT_DESCRIPTION);
+        }
+    }
 
 }
