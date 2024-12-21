4ce8f85d1193a678270e4f3167e7785ef2a97124
cubewhy <61075476+cubewhy@users.noreply.github.com>
2024-12-21 13:57:55 +0800

    feat: add event types

--- chat-resource/src/main/java/org/qbynet/chat/entity/Event.java
+++ chat-resource/src/main/java/org/qbynet/chat/entity/Event.java
@@ -8,10 +8,13 @@ public class Event {
     private String user; // targetUser
     private EventType type;
 
-    public static @NotNull Event create(@NotNull User user, EventType type) {
+    private Object data; // the addition data for the event object
+
+    public static @NotNull Event create(@NotNull User user, EventType type, Object data) {
         Event event = new Event();
         event.user = user.getId();
         event.type = type;
+        event.data = data;
         return event;
     }
 }
