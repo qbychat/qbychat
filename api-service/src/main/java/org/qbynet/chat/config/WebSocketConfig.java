9523645fb01a50a15921bfa087252c8e5d327c58
cubewhy <61075476+cubewhy@users.noreply.github.com>
2024-12-22 18:25:11 +0800

    feat: sockjs support

--- api-service/src/main/java/org/qbynet/chat/config/WebSocketConfig.java
+++ api-service/src/main/java/org/qbynet/chat/config/WebSocketConfig.java
@@ -13,6 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket").withSockJS();
+        registry.addEndpoint("/websocket/sockjs").withSockJS();
+        registry.addEndpoint("/websocket");
     }
 }
