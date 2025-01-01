9f75e5df74507829223b0bfd78612cf707687e9a
cubewhy <61075476+cubewhy@users.noreply.github.com>
2025-01-01 12:37:33 +0800

    feat: reset bot token via GraphQL

--- api-service/src/main/java/org/qbynet/chat/controller/graphql/BotController.java
+++ api-service/src/main/java/org/qbynet/chat/controller/graphql/BotController.java
@@ -1,7 +1,9 @@
 package org.qbynet.chat.controller.graphql;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.annotation.Resource;
 import org.jetbrains.annotations.NotNull;
+import org.qbynet.chat.entity.Bot;
 import org.qbynet.chat.entity.CreateBot;
 import org.qbynet.chat.entity.config.BotConfig;
 import org.qbynet.chat.entity.dto.CreateBotDTO;
@@ -29,7 +31,7 @@ public class BotController {
     @MutationMapping
     @Secured("SCOPE_bot.create")
     public BotVO createBot(@Argument CreateBotDTO input) {
-        if(!botConfig.isState()) {
+        if (!botConfig.isState()) {
             throw new IllegalStateException("Bots are disabled on this instance.");
         }
         CreateBot cb;
@@ -37,6 +39,25 @@ public class BotController {
             cb = userService.createBot(userService.currentUser(), input.getUsername(), input.getNickname());
             return BotVO.from(cb);
         } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @MutationMapping
+    @Secured("SCOPE_bot.manage")
+    public BotVO resetBotToken(@Argument String id) {
+        if (!botConfig.isState()) {
+            throw new IllegalStateException("Bots are disabled on this instance.");
+        }
+        Bot bot = userService.findBot(id);
+        if (!bot.isBelongTo(userService.currentUser())) {
+            throw new Forbidden("You are not allowed to reset this bot");
+        }
+        CreateBot cb;
+        try {
+            cb = userService.resetBotToken(bot);
+            return BotVO.from(cb);
+        } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
     }
